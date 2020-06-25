1ec0b7856d4ead0ff2e8ef8a5f416fd99437ab3a
Pinterid <david.pinterics@gmail.com>
2020-06-25 12:42:22 +0200

    Add oauth export
    The file for accessing the oauth components was added.

---
+++ src/index.js
@@ -0,0 +1,12 @@
+//#region > Imports
+import GitHubOAuth from "./GitHub";
+//#endregion
+
+//#region > Exports
+export default GitHubOAuth;
+//#endregion
+
+/**
+ * SPDX-License-Identifier: (EUPL-1.2)
+ * Copyright © 2019-2020 Simon Prast
+ */
